3404882b9355c169c7a0ff45204b20cf89ef5a0e
usamaa1 <usamariz1997@gmail.com>
2025-11-05 19:43:42 +0500

    till dotnet first project

---
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Net;
+
+Console.WriteLine("Give me Website URL:");
+
+string WebURL = Console.ReadLine();
+
+Console.WriteLine("Now give the file Name: ");
+
+string fileName = Console.ReadLine();
+
+WebClient client = new WebClient();
+string reply = client.DownloadString(WebURL);
+
+Console.WriteLine(reply);
+
+
+// Set a variable to the Documents path.
+string docPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+// Write the text to a new file named "WriteFile.txt".
+File.WriteAllText(Path.Combine(docPath, $"webs/{fileName}"), reply); //Desktop/webs/index.html
